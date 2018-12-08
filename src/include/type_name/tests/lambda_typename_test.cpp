cd653392fe58b4c7b981adfd759a3fdd03a237ac
Pascal Thomet <pthomet@gmail.com>
2018-12-08 16:21:13 +0100

    separate tests

--- src/include/type_name/tests/lambda_typename_test.cpp
+++ src/include/type_name/tests/lambda_typename_test.cpp
@@ -7,6 +7,40 @@
 
 #define LOG(str) std::cout << str << std::endl
 
+TEST_CASE("log_type_lambda_clean")
+{
+  {
+    auto f = []() { std::cout << "Hello"; };
+    REQUIRE_EQ(show_details_lambda(f), "[lambda: () -> void] f");
+  }
+  {
+    auto f = []() { return 42u; };
+    auto s = show_details_lambda(f);
+    REQUIRE_EQ(show_details_lambda(f), "[lambda: () -> unsigned int] f");
+  }
+  {
+    int c = 5;
+    auto f = [&c](int a, int b) -> double { return a + b + c; };
+    REQUIRE_EQ(show_details_lambda(f), "[lambda: (int, int) -> double] f");
+  }
+  {
+    int c = 5;
+    auto f = [](int a, int b) { return std::pair<int, double>(a + b, cos(a + static_cast<double>(b))); };
+#ifndef _MSC_VER // missing space after ','
+    REQUIRE_EQ(show_details_lambda(f), "[lambda: (int, int) -> std::pair<int, double>] f");
+#endif
+  }
+  {
+    std::string prefix = "a-";
+    auto f = [&prefix](const std::string &s) { return prefix + s; };
+    std::string expected1("[lambda: (std::string& const) -> std::string] f");
+    std::string expected2("[lambda: (std::string & const) -> std::string] f");
+    std::string computed = show_details_lambda(f);
+    REQUIRE(((computed == expected1) || (computed == expected2)));
+    // REQUIRE_EQ(show_details_lambda(f), "[lambda: (std::string& const) -> std::string] f");
+  }
+}
+
 
 ////////////// Internal tests below
 
---
+++ src/include/type_name/tests/type_name_clean_test.cpp
@@ -0,0 +1,89 @@
+#include "doctest.h"
+#include "type_name/type_name_clean.hpp"
+#include "type_name/details/lambda_typename.hpp"
+#include <fplus/fplus.hpp>
+#include <functional>
+#include <map>
+
+#define LOG(str) std::cout << str << std::endl
+
+TEST_CASE("log_var")
+{
+  {
+    int a = 1;
+    REQUIRE_EQ(log_var_str(a), "[int] a = 1");
+  }
+  {
+    std::string s("hello");
+    REQUIRE_EQ(log_var_str(s), "[std::string] s = hello");
+  }
+  {
+    std::vector<int> v{ { 1, 2, 3, 4, 5 } };
+    REQUIRE_EQ(log_var_str(v), "[std::vector<int>] v = [1, 2, 3, 4, 5]");
+  }
+  {
+    std::map<std::string, int> v{ {
+      { "a", 1 },
+      { "b", 2 },
+      { "c", 3 }
+      } };
+    auto t = log_var_str_cont(v);
+#ifndef _MSC_VER // WIP !!!             "[std::map<std::string,int>] v = [(a, 1), (b, 2), (c, 3)]"
+    REQUIRE_EQ(log_var_str_cont(v), "[std::map<std::string, int>] v = [(a, 1), (b, 2), (c, 3)]");
+#endif
+  }
+  {
+    std::vector<std::string> v{ {
+      { "a" },
+      { "b" },
+      { "c" }
+      } };
+    REQUIRE_EQ(log_var_str(v), "[std::vector<std::string>] v = [a, b, c]");
+  }
+  {
+    std::set<std::string> v{ {
+      { "a" },
+      { "b" },
+      { "c" }
+      } };
+    REQUIRE_EQ(log_var_str(v), "[std::set<std::string>] v = [a, b, c]");
+  }
+}
+
+
+
+TEST_CASE("demangle_test_fixme")
+{
+  // {
+  //     const std::vector<std::pair<std::string, int>> v;
+  //     //const auto &&vv = std::move(v);
+  //     std::string type_full = var_type_name_full(v);
+  //     std::string type_clean = type_name::demangle_typename(type_full);
+  //     std::cout << "type_full: " << type_full << "\n";
+  //     std::cout << "type_clean: " << type_clean << '\n';
+  // }
+  // {
+  //     std::cout << "\n";
+  //     const std::vector<std::pair<std::string, int>> v;
+  //     const auto &&vv = std::move(v);
+  //     auto s = var_type_name_full(vv);
+  //     std::cout << "Mangled:\n"
+  //               << s << "\n";
+  //     std::cout << "\n";
+  //     std::cout << "Demangled:\n"
+  //               << type_name::demangle_typename(s) << '\n';
+  // }
+
+  // {
+  //     std::cout << "\n";
+  //     auto f = [](const std::vector<std::string> &v, int a) {
+  //         return v.size() + a;
+  //     };
+  //     auto s = var_type_name_full(f);
+  //     std::cout << "Mangled:\n"
+  //               << s << "\n";
+  //     std::cout << "\n";
+  //     std::cout << "Demangled:\n"
+  //               << type_name::demangle_typename(s) << '\n';
+  // }
+}
