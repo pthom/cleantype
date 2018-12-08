a4a9d32b111f42fff23aab3d465abd73163b05ed
Pascal Thomet <pthomet@gmail.com>
2018-12-08 22:18:54 +0100

    docker_start ; enable debugging

--- scripts/docker_build/ubuntu18/docker_start.sh
+++ scripts/docker_build/ubuntu18/docker_start.sh
@@ -1,2 +1,6 @@
 DIR="$(dirname "$(greadlink -f "$0")")" # readlink on linux (brew install coreutils on mac)
-docker run --name u18builder -it -v $DIR/../../..:/sources_docker u18build /bin/zsh
+docker run \
+ --name u18builder -it -v $DIR/../../..:/sources_docker \
+--cap-add=SYS_PTRACE --security-opt seccomp=unconfined --security-opt apparmor=unconfined \
+u18build \
+/bin/zsh
