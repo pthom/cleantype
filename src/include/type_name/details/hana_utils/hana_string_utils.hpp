7cf2d7ce1e8fe9ff852f7446607e48df42b62af8
Pascal Thomet <pthomet@gmail.com>
2018-12-14 21:37:34 +0100

    inline generic lambdas only for msvc

--- src/include/type_name/details/hana_utils/hana_string_utils.hpp
+++ src/include/type_name/details/hana_utils/hana_string_utils.hpp
@@ -2,14 +2,18 @@
 
 #include <boost/hana.hpp>
 #include <boost/hana/reverse.hpp>
-//#include <boost/hana/fwd/reverse.hpp>
 #include <boost/hana/fwd/drop_while.hpp>
-//#include <boost/hana/for_each.hpp>
+
+#ifdef _MSC_VER
+#define TNS_INLINE_VARIABLES inline
+#else
+#define TNS_INLINE_VARIABLES
+#endif
 
 namespace hana_tn_utils {
 
 
-inline auto reverse_boost_hana_string = [](auto s) {
+TNS_INLINE_VARIABLES auto reverse_boost_hana_string = [](auto s) {
     auto add_reverse = [=](auto xs, auto ys) {
         auto ys_s = boost::hana::string<ys>();
         return ys_s + xs;
@@ -19,7 +23,7 @@ inline auto reverse_boost_hana_string = [](auto s) {
 };
 
 
-inline auto trim_string_left_c = [](auto trim_what, auto xs) {
+TNS_INLINE_VARIABLES auto trim_string_left_c = [](auto trim_what, auto xs) {
     auto should_trim = [&trim_what](auto x) {
         return x == trim_what;
     };
@@ -28,7 +32,7 @@ inline auto trim_string_left_c = [](auto trim_what, auto xs) {
 };
 
 
-inline auto trim_string_right_c = [](auto trim_what, auto xs) {
+TNS_INLINE_VARIABLES auto trim_string_right_c = [](auto trim_what, auto xs) {
     auto xs_r = reverse_boost_hana_string(xs);
     auto xs_r_trim = trim_string_left_c(trim_what, xs_r);
     auto trimmed = reverse_boost_hana_string(xs_r_trim);
@@ -36,26 +40,26 @@ inline auto trim_string_right_c = [](auto trim_what, auto xs) {
 };
 
 
-inline auto trim_string_c = [](auto trim_what, auto xs) {
+TNS_INLINE_VARIABLES auto trim_string_c = [](auto trim_what, auto xs) {
     auto x1 = trim_string_left_c(trim_what, xs);
     auto x2 = trim_string_right_c(trim_what, x1);
     return x2;
 };
 
 
-inline auto trim_string_left_space = [](auto xs) {
+TNS_INLINE_VARIABLES auto trim_string_left_space = [](auto xs) {
     auto space =  boost::hana::integral_constant< char, ' ' >();
     return trim_string_left_c(space, xs);
 };
 
 
-inline auto trim_string_right_space = [](auto xs) {
+TNS_INLINE_VARIABLES auto trim_string_right_space = [](auto xs) {
     auto space =  boost::hana::integral_constant< char, ' ' >();
     return trim_string_right_c(space, xs);
 };
 
 
-inline auto trim_string_space = [](auto xs) {
+TNS_INLINE_VARIABLES auto trim_string_space = [](auto xs) {
     auto space =  boost::hana::integral_constant< char, ' ' >();
     return trim_string_c(space, xs);
 };
