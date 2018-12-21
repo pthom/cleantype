05a4d618399563f206555e9ff2c085c4d7abbaf6
Pascal Thomet <pthomet@gmail.com>
2018-12-21 15:10:17 +0100

    clean_type_compiler_parser : add note

--- src/include/cleantype/compile_time/example/cleantype_compile_time_example.cpp
+++ src/include/cleantype/compile_time/example/cleantype_compile_time_example.cpp
@@ -11,7 +11,7 @@ auto my_spurious_lambda = [](int a, int b)
 };
 
 // Usage
-// WINDOWS : 
+// WINDOWS :
 //            cmake --build . | bin\cleantype_compiler_parser.exe -c
 // Clang:
 //            cmake --build . | ./cleantype_compiler_parser.exe -c
@@ -21,3 +21,28 @@ int main()
     TN_ERROR_full( decltype(c) );
     //cleantype::ERROR_full<decltype(c)>();
 }
+
+
+/*
+
+Clang example session:
+
+> ninja 2>&1 | ./cleantype_compiler_parser -c
+std::map<std::string, int>
+	at: ../src/include/cleantype/compile_time/example/cleantype_compile_time_example.cpp:21:5:
+    TN_ERROR_full( decltype(c) );
+    ^
+	Compiler line #4
+
+MSVC example session (in a msdos console, not a git bash console):
+
+> cmake --build . 2>&1 | bin\cleantype_compiler_parser.exe -c
+std::map<std::string, int>
+        at:   F:\dvp\OpenSource\type_name\src\include\cleantype\compile_time\example\cleantype_compile_time_example.cpp(16)
+        Compiler line #188
+
+F:\dvp\OpenSource\type_name\build> cmake --build . | bin\cleantype_compiler_parser.exe -c
+std::map<std::string, int>
+        at:   F:\dvp\OpenSource\type_name\src\include\cleantype\compile_time\example\cleantype_compile_time_example.cpp(16)
+        Compiler line #188
+*/
