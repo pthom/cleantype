32b70ec942c96b4b45c546870e707307aeadf043
Pascal Thomet <pthomet@gmail.com>
2019-01-02 14:39:54 +0100

    impl_lambda_generic : mergd version for C++ 14

--- src/include/cleantype/details/cleantype_lambda_generic.hpp
+++ src/include/cleantype/details/cleantype_lambda_generic.hpp
@@ -9,46 +9,32 @@ namespace cleantype
 {
     namespace internal
     {
-        // _impl_lamda_to_ptr cannot be extracted into a separate function!
-        // because its return type will be unknown to the compiler...
-        // ("error: cannot deduce return type 'auto' from returned value of type '<overloaded function type>'")
-        //
-        // template<typename... Args, typename Lambda>
-        // auto _impl_lamda_to_ptr()
-        // {
-        //     if constexpr((sizeof...(Args) > 0))
-        //         #ifdef _MSC_VER
-        //             return &Lambda::operator() < Args... > ;
-        //         #else
-        //             return &Lambda::template operator() < Args... > ;
-        //         #endif
-        //     else
-        //         return &Lambda::operator(); // if you have an error here, your lambda is generic! Add template params for its input types!
-        // }
-
-
-        template <typename... Args, typename Lambda> std::string impl_lambda_generic(Lambda fn, bool clean_type)
+        template <typename First, typename... Rest, typename Lambda>
+        std::string impl_lambda_generic(Lambda fn, bool clean_type)
         {
             std::string mem_fn_type;
             {
-                if constexpr((sizeof...(Args) > 0))
-                {
-                    #ifdef _MSC_VER
-                        auto as_ptr = &Lambda::operator() < Args... > ;
-                    #else
-                        auto as_ptr = &Lambda::template operator() < Args... > ;
-                    #endif
-                    auto as_mem_fn = std::mem_fn(as_ptr);
-                    mem_fn_type = cleantype::internal::_impl_typeid_hana<decltype(as_mem_fn)>();
-                }
-                else
-                {
-                    auto as_ptr = &Lambda::operator(); // if you have an error here, your lambda is generic! Add template params for its input types!
-                    auto as_mem_fn = std::mem_fn(as_ptr);
-                    mem_fn_type = cleantype::internal::_impl_typeid_hana<decltype(as_mem_fn)>();
-                }
+                #ifdef _MSC_VER
+                    auto as_ptr = &Lambda::operator() < First, Rest... > ;
+                #else
+                    auto as_ptr = &Lambda::template operator() < First, Rest... > ;
+                #endif
+                auto as_mem_fn = std::mem_fn(as_ptr);
+                mem_fn_type = cleantype::internal::_impl_typeid_hana<decltype(as_mem_fn)>();
             }
 
+            std::string final_type = cleantype::internal::_mem_fn_to_lambda_type(mem_fn_type, clean_type);
+            return final_type;
+        }
+
+        template <typename Lambda> std::string impl_lambda_generic(Lambda fn, bool clean_type)
+        {
+            std::string mem_fn_type;
+            {
+                auto as_ptr = &Lambda::operator(); // if you have an error here, your lambda is generic! Add template params for its input types!
+                auto as_mem_fn = std::mem_fn(as_ptr);
+                mem_fn_type = cleantype::internal::_impl_typeid_hana<decltype(as_mem_fn)>();
+            }
             std::string final_type = cleantype::internal::_mem_fn_to_lambda_type(mem_fn_type, clean_type);
             return final_type;
         }
