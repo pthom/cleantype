2e95c37977aa6d0005363e61dd53ae280b19ccb8
Pascal Thomet <pthomet@gmail.com>
2019-01-14 23:13:39 +0100

    added cleantype_test_indent.cpp

---
+++ src/include/cleantype/tests/cleantype_test_indent.cpp
@@ -0,0 +1,33 @@
+#include "doctest.h"
+#include <cleantype/cleantype.hpp>
+
+#define LOG(...) std::cout << __VA_ARGS__ << "\n";
+#define LOG_VAR(...) std::cout << #__VA_ARGS__ << "\n" << __VA_ARGS__ << "\n";
+
+namespace wip_indent
+{
+        inline std::string code_pair_tree_to_string(cleantype::internal::code_pair_tree const & xs)
+        {
+            return fp::fp_add::show_tree_lhs_rhs(
+                    xs,
+                    cleantype::internal::make_template_tree_separators(),
+                    cleantype::internal::make_template_show_tree_options_impl(true));
+        }
+
+
+    TEST_CASE("indent")
+    {
+        // {
+        //     std::string s("ranges::v3::single_view<int, double>");
+        //     cleantype::internal::code_pair_tree template_tree = cleantype::internal::parse_template_tree(s);
+        //     auto s2 = code_pair_tree_to_string(template_tree);
+        //     LOG_VAR(s2);
+        // }
+        {
+            std::string s("ranges::v3::detail::take_exactly_view_<ranges::v3::join_view<ranges::v3::transform_view<ranges::v3::iota_view<int, void>, (lambda at ../src/include/cleantype/tests/cleantype_rangev3_test.cpp:97:25)>, void>, false> &");
+            cleantype::internal::code_pair_tree template_tree = cleantype::internal::parse_template_tree(s);
+            auto s2 = code_pair_tree_to_string(template_tree);
+            LOG_VAR(s2);
+        }
+    }
+}
