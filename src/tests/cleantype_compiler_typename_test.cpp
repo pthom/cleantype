9d3fca05efe31b2e72c0544430fdfaef47a23db8
Pascal Thomet <pthomet@gmail.com>
2019-01-18 00:05:00 +0100

    added test cleantype_compiler_typename_test

---
+++ src/tests/cleantype_compiler_typename_test.cpp
@@ -0,0 +1,16 @@
+// This file is part of cleantype: Clean Types for C++
+// Copyright Pascal Thomet - 2018
+// Distributed under the Boost Software License, Version 1.0. (see LICENSE.md)
+#include "doctest.h"
+#include <cleantype/cleantype.hpp>
+#include <cleantype/cleantype_compiler_typename.hpp>
+#include <cleantype/details/debug_utilities.hpp>
+
+TEST_CASE("cleantype_compiler_typename_test")
+{
+    auto int_type = cleantype::full_compiletime<int>();
+    REQUIRE_EQ(cleantype::full(int_type), "boost::hana::string<'i', 'n', 't'> &");
+
+    // Note : there are lots of other tests inside
+    // src/include/cleantype/details/hana_type_name/tests/type_name_stringliteral_test.cpp
+}
