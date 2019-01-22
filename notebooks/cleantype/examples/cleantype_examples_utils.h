d06bd83855cd0f844496663bccd1df3d3e42dfcd
Pascal Thomet <pthomet@gmail.com>
2019-01-22 21:14:33 +0100

    added quick_demo.ipynb

--- notebooks/cleantype/examples/cleantype_examples_utils.h
+++ notebooks/cleantype/examples/cleantype_examples_utils.h
@@ -2,17 +2,48 @@
 #pragma cling add_include_path("../include")
 #include <cleantype/cleantype.hpp>
 
-#include <vector>
-#include <map>
 #include <array>
-//#include <future>
+#include <fplus/fplus.hpp>
+#include <map>
 #include <numeric>
-//#include <fplus/fplus.hpp>
-//#include <range/v3/all.hpp>
+#include <vector>
 
 // This macro is used later in this manual in order to display commands and their results
-#define run_show(...)                   \
-{                                       \
-    std::cout << #__VA_ARGS__ << "\n";  \
-    std::cout << __VA_ARGS__ << "\n\n"; \
+#define run_show(...)                       \
+    {                                       \
+        std::cout << #__VA_ARGS__ << "\n";  \
+        std::cout << __VA_ARGS__ << "\n\n"; \
+    }
+
+std::string int_to_roman(int value)
+{
+    // clang-format off
+    std::vector<std::pair<int, char const*>> roman
+    {
+      { 1000, "M" },{ 900, "CM" },
+      { 500, "D" },{ 400, "CD" },
+      { 100, "C" },{ 90, "XC" },
+      { 50, "L" },{ 40, "XL" },
+      { 10, "X" },{ 9, "IX" },
+      { 5, "V" },{ 4, "IV" },
+      { 1, "I" }
+    };
+    // clang-format on
+    std::string result;
+    for (auto const & kv : roman)
+    {
+        while (value >= kv.first)
+        {
+            result += kv.second;
+            value -= kv.first;
+        }
+    }
+    return result;
+}
+
+std::string int_to_string(int value)
+{
+    std::stringstream ss;
+    ss << value;
+    return ss.str();
 }
